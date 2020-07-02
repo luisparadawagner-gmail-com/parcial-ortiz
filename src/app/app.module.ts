91029119dea02f97ca99a3e1b77860e03192272e
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2020-07-01 21:38:46 -0300

    Se crea Formulario de alumnos
    Se crea el formulario para la carga de alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { FormularioAlumnoComponent } from './componentes/formulario-alumno/formulario-alumno.component';
 import { TablaAlumnoComponent } from './componentes/tabla-alumno/tabla-alumno.component';
 
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormularioAlumnoComponent,
-    TablaAlumnoComponent
-  ],
+  declarations: [AppComponent, FormularioAlumnoComponent, TablaAlumnoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/componentes/formulario-alumno/formulario-alumno.component.ts
+++ src/app/componentes/formulario-alumno/formulario-alumno.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+interface Materia {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-formulario-alumno',
   templateUrl: './formulario-alumno.component.html',
-  styleUrls: ['./formulario-alumno.component.css']
+  styleUrls: ['./formulario-alumno.component.css'],
 })
 export class FormularioAlumnoComponent implements OnInit {
+  materias: Materia[] = [
+    { value: '0', viewValue: 'Mongo DB' },
+    { value: '1', viewValue: 'JavaScript' },
+    { value: '2', viewValue: 'NodeJS' },
+    { value: '3', viewValue: 'ExpressJS' },
+  ];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  alumnoForm = this.fb.group({
+    nombre: [''],
+    apellido: [''],
+    edad: [''],
+    materia: [''],
+    anoCursada: [''],
+  });
+
+  ngOnInit(): void {}
+
+  submit() {
+    debugger;
+    this.alumnoForm.value;
   }
-
 }
